85bfc8406fe95da9ceaec9ce41b6526627ff66a8
burke212 <burke212@live.com>
2018-04-10 10:56:39 -0400

    HW4 - Implemented DoG
    Implemented Difference of Gaussian function.
    
    DoG() reads in images saved from calculateOctaves(), calculates the DoG's, then saves the outputs to a DoG folder.

--- SIFT01/OpenCV_Retest/Source.cpp
+++ SIFT01/OpenCV_Retest/Source.cpp
@@ -55,7 +55,7 @@ void calculateOctave1(Mat source) {
 			
 			//imshow(layer, dest);
 			imshow(layer, temp);
-			imwrite(layer+".png", temp);
+			imwrite("Octaves/" + layer + ".png", temp);
 
 			kern = kern + Size(2,2);// Increase the kernel size to 3 5 7 9 11
 		}
@@ -65,20 +65,61 @@ void calculateOctave1(Mat source) {
 		resize(dest, dest, Size(dest.cols*0.5, dest.rows*0.5), 0.5, 0.5); // Resize the image | half the image size. 
 	}
 
-
 	waitKey(0);
+}
+
+
+int calulateDoG() {
+	String DOG;
+	
+	Mat DoG;
+
+	for (int i = 1; i < 5; i++) {
+		for (int j = 1; j < 5; j++) {
+			String octave = "Octave" + to_string(i);// make string for each Octave, i.e. "Octave 1", "Octave 2"...
+			String layer = octave + "_" + "Layer";// +to_string(j);// make string for each Octave, i.e. "layer 1", "layer 2"...
+
+			Mat img1 = imread("Octaves/" + layer + to_string(j) + ".png", CV_LOAD_IMAGE_COLOR);// i.e. "Octave"i_"Layer"j
+			Mat img2 = imread("Octaves/" + layer + to_string(j + 1) + ".png", CV_LOAD_IMAGE_COLOR);// i.e. "Octave"i_"Layer"j+1
+			
+			// Validate image isn't empty
+			if (img1.empty()) {
+				cout << "Can't load img1";
+				waitKey(0);
+				return -1;
+			}
+
+			// Validate image isn't empty
+			if (img2.empty()) {
+				cout << "Can't load img2";
+				waitKey(0);
+				return -1;
+			}
+
+			DoG = img1 - img2;
+			
+			DOG = "DoG/DOG" + to_string(i) + "_" + layer + to_string(j) + ".png";// "DoG"i"_Octave"i"_Layer"j
+			
+			//imshow(layer, DoG); // Uncomment to view DoG output.
+			imwrite(DOG, DoG);
+		}
+	}
+	waitKey(0);
+	return 0;
 }
 
 
 int main(int argc, char argv[])
 {
 	// Show the original image.
-	showOriginalImg();
+	//showOriginalImg();
 	
 	// Apply 3x3 gauss on original image
 	//applyGuassianBlur(original_img, kern3x3);
 
 	calculateOctave1(original_img);
+
+	calulateDoG();
 
 	return 0;
 }
