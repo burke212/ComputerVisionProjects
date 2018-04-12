76b15553d3da1985bad218b267ac667b5bfe4b76
burke212 <burke212@live.com>
2018-04-12 11:10:36 -0400

    HW4 - Fixed FindMinMax()
    So remember when I said the MinMaxLoc() didn't work b/c it was finding multiple min/max values in multiple locations per image, and thus wasn't working? Well, apparently the real reason was b/c I had the image as the wrong type. MinMaxLoc() doesn't work w/ multi channel arrays (RGB), as I found out. So when I convert the img --> grayscale, it works! Now it finds the min, max, and their locations.

--- SIFT01/OpenCV_Retest/Source.cpp
+++ SIFT01/OpenCV_Retest/Source.cpp
@@ -108,59 +108,74 @@ int calulateDoG() {
 	return 0;
 }
 
-void findMax() {
-	String DOG;
-	double minVal; double maxVal; Point minLoc; Point maxLoc;
-	int count = 1;
-	vector<Point> max_points;
-	vector<Point> min_points;
+string type2str(int type) {
+	string r;
 
-	for (int i = 1; i < 5; i++) {
-		for (int j = 1; j < 5; j++) {
+	uchar depth = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+	switch (depth) {
+	case CV_8U:  r = "8U"; break;
+	case CV_8S:  r = "8S"; break;
+	case CV_16U: r = "16U"; break;
+	case CV_16S: r = "16S"; break;
+	case CV_32S: r = "32S"; break;
+	case CV_32F: r = "32F"; break;
+	case CV_64F: r = "64F"; break;
+	default:     r = "User"; break;
+	}
+
+	r += "C";
+	r += (chans + '0');
+
+	return r;
+}
+void findMinMax() {
+	String DOG; // Text to hold DoG picture name
+	double minVal; double maxVal; Point minLoc; Point maxLoc; // Values for Max/ Min
+	int count = 1;//TEST
+	vector<Point> max_points;// Hold max val locations
+	vector<Point> min_points;// Hold min val locations
+
+	for (int i = 1; i < 5; i++) {// Iterate thru Octaves
+		for (int j = 1; j < 5; j++) {// Iterate thru (DoG) layers in Octaves
+
 			String octave = "Octave" + to_string(i);// make string for each Octave, i.e. "Octave 1", "Octave 2"...
 			String layer = octave + "_" + "Layer";// +to_string(j);// make string for each Octave, i.e. "layer 1", "layer 2"...
 
 			DOG = "DoG/DOG" + to_string(i) + "_" + layer + to_string(j) + ".png";// "DoG"i"_Octave"i"_Layer"j
 
 			Mat img1 = imread(DOG, CV_LOAD_IMAGE_COLOR);// i.e. "Octave"i_"Layer"j
-			//Mat img2 = imread(DOG, CV_LOAD_IMAGE_COLOR);// i.e. "Octave"i_"Layer"j+1
 			Mat max_img1 = Mat::zeros(img1.size(), CV_64FC1);
 			Mat min_img1 = Mat::zeros(img1.size(), CV_64FC1);
 			
-			// Validate image isn't empty
+			cvtColor(img1, img1, COLOR_RGB2GRAY);// Convert img from RGB -> grayscale
+
+			// Validate image loaded
 			if (img1.empty()) {
 				cout << "Can't load img1";
 				waitKey(0);
 				//	return -1;
 			}
 
-			// Validate image isn't empty
-			//if (img2.empty()) {
-			//	cout << "Can't load img2";
-			//	waitKey(0);
-			//	//	return -1;
-			//}
+			minMaxLoc(img1, &minVal, &maxVal, &minLoc, &maxLoc);// Find max & min value in image
 
-			minMaxLoc(img1, &minVal, &maxVal);// Find max & min value in image
-
-			if (img1.at<Point>(i,j) == maxVal) {
-				max_points.push_back(Point(i, j));
-				cout << "Pushed back: " << i << ", " << j << endl;
-			}
-			
-
-			/*if (img1.at<uchar>(Point(i, j)) == minVal) {
-				min_points.pushback(Point(i,j));
-			}*/
-
-			cout << "iteration: " << count << endl 
+			//TEST
+			cout << "iteration: " << count << endl
 				<< "	minval: " << minVal << endl
-				<< "	maxval: " << maxVal << endl;
+				<< "	maxval: " << maxVal << endl
+				<< "	minLoc: " << minLoc << endl
+				<< "	maxLoc: " << maxLoc << endl;
 			count++;
 		}
 	}
 	waitKey(0);
 }
+
+
+
+
+
 
 int main(int argc, char argv[])
 {
@@ -174,7 +189,7 @@ int main(int argc, char argv[])
 
 	calulateDoG();
 
-	findMax();
+	findMinMax();
 
 	return 0;
 }
